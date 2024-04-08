fec405f8f2f91f3449b9dee76f8ccbf5471ae32e
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2024-04-08 16:43:32 +0100

    removed checking

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -32,7 +32,6 @@ export default async function ProtectedPage() {
 
       <div className="animate-in flex-1 flex flex-row max-w-[100] px-3 h-full bg-background text-foreground justify-center items-center relative">
         <main className="flex-1 flex flex-row max-w-screen-xl w-full h-full relative ">
-          <p>creating nav branch</p>
           <Navigation />
           <ContentPage />
           <Explore />
