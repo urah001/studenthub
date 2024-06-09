74a5c4959284bb4367c35ccc90e405f528d42c58
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2024-06-09 07:59:02 +0100

    littlechange

--- app/gist/meta.tsx
+++ app/gist/meta.tsx
@@ -1,22 +1,5 @@
 "use server";
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supbaseSecretKey = process.env.SUPABASE_SECRET_KEY;
-
-// export const userData = async () => {
-//   const supabaseUser = createClient();
-//   const { data, error } = await supabaseUser.auth.getUser();
-//   if (error) {
-//     console.error("error fetching user data:", error);
-//     return null;
-//   }
-//   return data;
-// };
-
-// import { createClient } from "@supabase/supabase/server";
-
-import { Database } from "@/types/supabase";
-import { createClient } from "@/utils/supabase/server";
 import { SupabaseClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supbaseSecretKey = process.env.SUPABASE_SECRET_KEY;
