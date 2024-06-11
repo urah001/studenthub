ccbe42d71480aafa83636ac90539fb0da1bf042f
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2024-06-11 16:47:54 +0100

    removed comment

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,9 +1,3 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
-import { redirect } from "next/navigation";
 import Navigation from "@/components/Navigation";
 import ContentPage from "@/app/content/ContentPage";
 import Explore from "@/components/Explore";
