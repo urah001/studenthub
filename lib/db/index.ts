7be60cb4d2e1519ddafb9203ff8858510904643c
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2024-07-29 12:49:04 +0100

    changed site location

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,3 +1,7 @@
 import Pool from "pg-pool";
 
-const pool = new Pool({});
+const pool = new Pool({
+  database: "progres",
+  connectionString: process.env.DATABASE_CONNECTION_STRING,
+  allowExitOnIdle: true,
+});
