c8cb956aff3a95b6bf66509f67fa3a407bdc8f20
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2024-08-15 15:20:23 +0100

    fixed explore page

--- components/Explore.tsx
+++ components/Explore.tsx
@@ -3,32 +3,35 @@ import { IoIosSearch } from "react-icons/io";
 
 const Explore = () => {
   return (
+    /*  new style  */
     <div className="hidden lg:flex w-[20%] h-full justify-center text-foreground items-center relative bg-inherit">
       <div className="max-w-screen-xl w-full h-full flex relative">
-        <section className="fixed flex flex-col space-x-4 w-[275px] lg:w-[275px] xl:w-[300px]">
-          <div className="relative h-full w-full pl-2 group">
+        <section className="fixed flex flex-col space-x-4 w-[275px] lg:w-[275px] xl:w-[300px] mt-4">
+          <div className="relative h-full w-[80%] pl-2 group">
             <label
               htmlFor="searchBox"
               className="absolute top-0 left-0 h-full flex items-center pl-3 pointer-events-none"
             >
-              <IoIosSearch className="w-5 h-5 text-foreground" />
+              <IoIosSearch className="w-5 h-5 text-neutral-500 group-focus-within:text-primary transition duration-200" />
             </label>
             <input
               id="searchBox"
               type="text"
-              placeholder="search"
-              className="w-full h-full pl-10 pr-4 focus:border-primary focus:border outline-none rounded-md bg-inherit border mb-6"
+              placeholder="Search"
+              className="w-full h-10 pl-10 pr-4 focus:ring-2 focus:ring-primary focus:outline-none rounded-md border border-background text-white placeholder-neutral-500 transition duration-200 bg-inherit"
             />
           </div>
-          <div className="flex flex-col bg-background-900/90 rounded-xl mt-6">
-            <h3 className="font-bold text-2xl my-2">Trending Topic</h3>
+          <div className="flex flex-col bg-background-900/90 rounded-xl mt-6 p-4">
+            <h3 className="font-bold text-2xl my-2">Trending Topics</h3>
             <div className="space-y-4">
               {Array.from({ length: 5 }).map((_, i) => (
                 <div
                   key={i}
-                  className="hover:bg-background rounded-xl p-2 transition duration-100"
+                  className="hover:bg-background-800 rounded-xl p-2 transition duration-100 cursor-pointer"
                 >
-                  <div className="font-bold text-lg">#trends {i + 1}</div>
+                  <div className="font-bold text-lg text-white">
+                    #trends {i + 1}
+                  </div>
                   <div className="text-xs text-neutral-400 font-bold">30k</div>
                 </div>
               ))}
