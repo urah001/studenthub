51b3aee31205858a1d2341b6906f6e1d5b3cc9da
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2024-12-31 14:15:06 +0100

    made little changes

--- app/gist/queries.tsx
+++ app/gist/queries.tsx
@@ -4,9 +4,8 @@ import { createSupabase } from ".";
 import { revalidatePath } from "next/cache";
 import { pool } from "@/lib";
 import { db } from "@/lib/db";
-import { gists, likes, profiles } from "@/lib/db/schema";
+import { gists, gistsReplies, likes, profiles, replies } from "@/lib/db/schema";
 import { desc, eq } from "drizzle-orm";
-import { error } from "console";
 
 export type GistType = Database["public"]["Tables"]["gists"]["Row"] & {
   username: string;
@@ -14,44 +13,7 @@ export type GistType = Database["public"]["Tables"]["gists"]["Row"] & {
 };
 
 export const getGist = async (currentUserId?: string) => {
-  {
-    /* const query = `
-    SELECT
-      gists.*,
-      profiles.username,
-      profiles.full_name,
-      COUNT(likes.id) AS likes_count,
-      EXISTS(
-          SELECT 1
-          FROM likes
-          WHERE likes.gist_id = gists.id
-          AND likes.user_id = $1
-      ) AS user_has_liked
-    FROM
-      gists
-    LEFT JOIN
-      likes ON gists.id = likes.gist_id
-    LEFT JOIN
-      profiles ON gists.profile_id = profiles.id -- Link profiles to the gist's creator
-    GROUP BY
-      gists.id, profiles.username, profiles.full_name
-    ORDER BY
-      gists.created_at DESC;
-  `;
-  */
-  }
   try {
-    //  const res = await pool.query(query, [currentUserId]);
-    // const res = await db.query.gists.findMany({
-    //   with:{
-    //     profile:{
-    //     columns:{
-    //       username:true,
-    //       fullName:true
-    //     }
-    //     }
-    //   }
-    // })
     let err = "";
     const res = await db
       .select()
@@ -73,6 +35,31 @@ export const getGist = async (currentUserId?: string) => {
     return { error };
   }
 };
+{/*export const getComment = async (postId?: string) => {
+  try {
+    let err = "";
+    const res = await db
+      .select()
+      .from(gistsReplies)
+      .innerJoin(gists, eq(gistsReplies.replyId, gists.id)) // Join to filter replies for a specific post
+      .leftJoin(likes, eq(gistsReplies.id, likes.gistId)) // Join for likes data
+      .innerJoin(profiles, eq(gistsReplies.profileId, profiles.id)) // Join for profile data
+      .where(eq(gists.id, postId)) // Filter by the specific post ID
+      .orderBy(desc(gistsReplies.createdAt)) // Order by creation date
+      .limit(5)
+      .catch(() => {
+        err = "something went wrong while fetching all the gist";
+      });
+    //revalidatePath("/");
+    //console.log("response");
+
+    return { data: res, error: err };
+    // return { data: res.rows };
+  } catch (error) {
+    console.log(error);
+    return { error };
+  }
+};*/}
 
 /*export const getComment = async (postId: string) => {
   const { supabaseServer } = createSupabase();
